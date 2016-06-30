641dbb9ecbc095d4ec6f7e719b808e115b22b54a
Gregory HAVENGA <ghavenga@e5r5p2.jnb.42.fr>
2016-06-30 07:31:17 +0200

    Yesterdays work

--- Checker/main.c
+++ Checker/main.c
@@ -6,7 +6,7 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/27 07:45:11 by oexall            #+#    #+#             */
-/*   Updated: 2016/06/29 14:52:50 by ghavenga         ###   ########.fr       */
+/*   Updated: 2016/06/29 15:57:24 by ghavenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,58 @@ int				checkstack(int *stack, int ncount)
 	result = -1;
 	while (result = -1)
 
-	if (i < ncount && stack[i] > stack[i + 1])
-
-
 	result = 1;
 	
 }
 
+void			execute(char *line, t_stack a, t_stack b)
+{
+	if (ft_strcmp(line, "sa")
+		if (line[1] == "sb")
+		else if (line[1] == "ss")
+		else if (line[1] == "pa")
+	else if (line[0] == "pb")
+		if (line[1] == "pp")
+		else if (line[1] == 'b')
+	else if (line[0] == 'r')
+		if (line[1] == 'a')
+		else if (line[1] == 'b')
+		else if (line[1] == 'r')
+			if (line[2] == '\0')
+			else if (line[2] == 'a')
+			else if (line[2] == 'b')
+			else if (line[2] == 'r')
+
+}
+
 int				main(int argc, char **argv)
 {
-	t_stack		stack;
+	t_stack		a;
+	t_stack		b;
 	int			ncount;
 	char		*line;
-	int			res;
+	int			i;
+	int			k;
 	
-	res = -1;
+	i = 0;
+	k = 0;
 	if (argc > 1 && argv)
 	{
+		//checks args
 		ncount = argc - 1;
-		stack.a = malloc(sizeof(int) * ncount);
-		stack.b = malloc(sizeof(int) * ncount);
+		while (++i <= ncount + 1)
+			while (++k <= ncount + k + 1)
+				if (argv[i] == argv[k] || argv[i] > INT_MAX)
+					ft_puterror("Error");
+
+		a.stk = malloc(sizeof(int) * ncount);
+		a.len = ncount;
+		a.last = ncount;
+		b.stk = malloc(sizeof(int) * ncount);
+		b.len = ncount;
+		b.last = -1;
 		while (++res <= ncount)
 			stack.a[res] = argv[res + 1];
-		if (checkstack(stack.a, argv) == 3)
-			ft_printf("Invalid Arguements\n");
 		while (get_next_line(1, &line));
 		execute(&line, &stack);
 		res = checkstack(stack.a);
